f542bad1acc5e0506341561ad6796b714f17ddc1
interdisciplinar <jeanpsantos@unifebe.edu.br>
2017-10-20 23:07:45 -0300

    Tela de Validação Máscaras nos campos de telefone

--- app/src/main/java/br/com/wapp/cursoandroid/wapp/activity/LoginActivity.java
+++ app/src/main/java/br/com/wapp/cursoandroid/wapp/activity/LoginActivity.java
@@ -2,14 +2,45 @@ package br.com.wapp.cursoandroid.wapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
 import br.com.wapp.cursoandroid.wapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText telefone;
+    private EditText codPais;
+    private EditText codArea;
+    private EditText nome;
+    private Button cadastrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        telefone = (EditText) findViewById(R.id.edit_telefone);
+        nome = (EditText) findViewById(R.id.edit_nome);
+        codArea = (EditText) findViewById(R.id.edit_cod_area);
+        codPais = (EditText) findViewById(R.id.edit_cod_pais);
+        cadastrar = (Button) findViewById(R.id.bt_cadastrar);
+
+
+        //Definição das máscaras
+        SimpleMaskFormatter simpleMaskCodPais = new SimpleMaskFormatter("+NN");
+        SimpleMaskFormatter simpleMaskCodArea = new SimpleMaskFormatter("NN");
+        SimpleMaskFormatter simpleMaskTelefone = new SimpleMaskFormatter("NNNNN-NNNN");
+
+        MaskTextWatcher maskCodPais = new MaskTextWatcher(codPais, simpleMaskCodPais);
+        MaskTextWatcher maskCodArea = new MaskTextWatcher(codArea, simpleMaskCodArea);
+        MaskTextWatcher maskTelefone = new MaskTextWatcher(telefone, simpleMaskTelefone);
+
+        codPais.addTextChangedListener(maskCodPais);
+        codArea.addTextChangedListener(maskCodArea);
+        telefone.addTextChangedListener(maskTelefone);
     }
 }
